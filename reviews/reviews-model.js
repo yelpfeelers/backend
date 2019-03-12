bbb84a6b8ef397b33a6cec915c004b5cddcc9216
john2796 <jbmiranda22796@gmail.com>
2019-03-11 22:27:55 -0700

    Implementing reviews

---
+++ reviews/reviews-model.js
@@ -0,0 +1,63 @@
+const db = require('../data/dbConfig');
+// get all & query name & pagination
+// query by firstname
+function find(query) {
+  const { limit = 10, page = 1, name } = query;
+  let table;
+  if (!name) {
+    table = db
+      .select()
+      .from('reviews')
+      .orderBy('id', 'desc')
+      .paginate(limit, page, true);
+  } else {
+    table = db
+      .select()
+      .from('reviews')
+      .orderBy('id', 'desc')
+      .where('alias', 'like', `%${name}%`)
+      .paginate(limit, page, true);
+  }
+
+  return table;
+}
+// find by id
+function findById(id) {
+  return db
+    .select()
+    .from('reviews')
+    .where(id)
+    .first();
+}
+function findBy(query) {
+  return db
+    .select()
+    .from('reviews')
+    .where(query)
+    .first();
+}
+function insert(post) {
+  return db.insert(post).into('reviews').returning("id");
+}
+function update(id, changes) {
+  return db
+    .update(changes)
+    .from('reviews')
+    .where(id);
+}
+function remove(id) {
+  return db
+    .del()
+    .from('reviews')
+    .where(id);
+}
+
+
+module.exports = {
+  find,
+  findById,
+  insert,
+  update,
+  remove,
+  findBy,
+};
