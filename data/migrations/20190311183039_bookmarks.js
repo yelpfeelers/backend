770b4612a687e758cb13c8de3f19a9bc6d5a03cf
john2796 <jbmiranda22796@gmail.com>
2019-03-11 19:09:49 -0700

    created migration for bookmarks and user_bookmarks

--- data/migrations/20190311183039_bookmarks.js
+++ data/migrations/20190311183039_bookmarks.js
@@ -1,7 +1,21 @@
 
 exports.up = (knex) => {
   return knex.schema.createTable('bookmarks', tbl => {
-    tbl.increments()
+    tbl.increments();
+    tbl.integer('business_id');
+    tbl.string('alias');
+    tbl.string('image_url');
+    tbl.boolean('is_closed');
+    tbl.string('categories');
+    tbl.integer('rating').unsigned()
+    tbl.integer('latitude');
+    tbl.integer('longitude');
+    tbl.string('transactions');
+    tbl.string('price');
+    tbl.string('location');
+    tbl.string('display_phone');
+
+
   })
 };
 
